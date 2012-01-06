3a23d76c3f34bd4718aadb9c17dd1ecf12bcc9af
Michiel de Jong <michiel@unhosted.org>
2012-01-06 17:08:24 +0700

    same for createDb

--- createDb/handler.js
+++ createDb/handler.js
@@ -4,6 +4,9 @@ exports.handler = (function() {
     querystring = require('querystring'),
     browseridVerify = require('browserid-verifier');
 
+  function checkToken(userAddress, token, cb) {
+    cb();
+  }
   function serve(req, res, baseDir) {
     var dataStr = '';
     req.on('data', function(chunk) {
@@ -12,51 +15,37 @@ exports.handler = (function() {
     req.on('end', function() {
       var incoming = JSON.parse(dataStr);
       console.log(incoming);
-      postData = {
-        audience: 'http://libredocs.org',
-        assertion: incoming.browserIdAssertion
-      };
-      console.log(postData);
-      browseridVerify(postData, function(err, r) {
-        if(err) {
-          res.writeHead(200, {'Content-type': 'application/json'});
-          res.write(JSON.stringify(err));
-          res.end();
-        } else {
-          if(r.email) {
-            console.log(r.email+' confirmed by browserid verifier');
-            var options = {
-              host: incoming.host,
-              port: 443,
-              path: '/'+incoming.dbName+'/',
-              method: 'PUT',
-              headers: {
-                'Authorization': req.headers.authorization,
-                'Content-Type': 'application/json'
-              }
-            };
-            console.log(options);
-            var request = https.request(options, function(response) {
-              console.log('STATUS: ' + response.statusCode);
-              console.log('HEADERS: ' + JSON.stringify(response.headers));
-              response.setEncoding('utf8');
-              var resStr = '';
-              response.on('data', function (chunk) {
-                resStr += chunk;
-                console.log('BODY: ' + chunk);
-              });
-              response.on('end', function() {
-                console.log('END');
-                res.writeHead(response.statusCode, response.headers);
-                res.write(resStr);
-                res.end();
-              });
-            });
-            console.log('ending the request');
-            request.end();
-            console.log('setting request.on(\'response\', ...)');
+      checkToken(incoming.userAddress, incoming.token, function() {
+        var options = {
+          host: incoming.host,
+          port: 443,
+          path: '/'+incoming.dbName+'/',
+          method: 'PUT',
+          headers: {
+            'Authorization': req.headers.authorization,
+            'Content-Type': 'application/json'
           }
-        }
+        };
+        console.log(options);
+        var request = https.request(options, function(response) {
+          console.log('STATUS: ' + response.statusCode);
+          console.log('HEADERS: ' + JSON.stringify(response.headers));
+          response.setEncoding('utf8');
+          var resStr = '';
+          response.on('data', function (chunk) {
+            resStr += chunk;
+            console.log('BODY: ' + chunk);
+          });
+          response.on('end', function() {
+            console.log('END');
+            res.writeHead(response.statusCode, response.headers);
+            res.write(resStr);
+            res.end();
+          });
+        });
+        console.log('ending the request');
+        request.end();
+        console.log('setting request.on(\'response\', ...)');
       });
     });
   }
