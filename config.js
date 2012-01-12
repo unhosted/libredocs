ed7eb6e2d08bfaff4edec28d423123d4cf625941
Michiel de Jong <michiel@unhosted.org>
2012-01-12 14:17:24 +0200

    update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 exports.config = {
   port: 80,
   redirect: {
+    'libredocs.nodejitsu.com': 'http://libredocs.org',
     'www.libredocs.org': 'http://libredocs.org',
     'www.libredoc.org': 'http://libredocs.org',
     'libredoc.org': 'http://libredocs.org'
@@ -9,6 +10,7 @@ exports.config = {
     'libredocs.org': '/static'
   },
   pathHandler: {
+    'libredocs.org/users': '/Users/mich/Code/Node/libredocs/users',
     'libredocs.org/browserid-verifier': './browserid',
     'libredocs.org/provision': './provision',
     'libredocs.org/squat': './squat',
