b1483c8a2a4701d44023d0b639a5a009d428510b
Michiel de Jong <michiel@unhosted.org>
2012-01-13 23:46:24 +0200

    split js from /write/index.html into /write/index.js

---
+++ static/write/index.js
@@ -0,0 +1,44 @@
+// this is probably not working anymore
+window.addEventListener('storage', function(e) {
+  if(e.key=='LibreDocsState') {
+    console.log('LibreDocsState '+e.newValue+' received');
+    if(e.newValue == 'exit') {
+      window.close();
+    } else if(e.newValue == 'offline') {
+      document.getElementById('spinner').style.display='block';
+    } else {
+      document.getElementById('spinner').style.display='none';
+    }
+  } else if(e.key == 'LibreDocsUserAddress') {
+    connectToOwnpad();
+  }
+}, false);
+// So far this will connect to the default etherpad server.
+// This is just a test for the editor embedding
+function connectToOwnpad() {
+  var userName, padId;
+  var sessionObj = JSON.parse(localStorage.getItem('sessionObj'));
+  if(sessionObj.userAddress != null) {
+    userName = sessionObj.userAddress;
+    } else {
+    userName = 'Address Not Set Yet'
+  }
+  if(sessionObj.currDocId != null) {
+    padId = sessionObj.currDocId;
+    } else {
+    padId = 'still-hosted-no-name'
+  }
+  $('#editorPad').pad({
+    'padId':padId,
+    'host':'http://ownpad.nodejitsu.com',
+    'storageAddress':sessionObj.storageAddress,
+    'bearerToken':sessionObj.bearerToken,
+    'storageApi':sessionObj.storageApi,
+    'userName':userName,
+    'showControls':true,
+    'showLineNumbers':true,
+    //we'll hopefully send the storage data here at some point.
+  });
+}
+document.getElementById('body').setAttribute('onload', 'connectToOwnpad();');
+document.getElementById('loading').style.display='none';
