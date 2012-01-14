cba65ba11b8a098c6a382f183fedabc1e0a5caf2
Michiel de Jong <michiel@unhosted.org>
2012-01-14 20:33:42 +0200

    put functions in logical order

--- static/remoteStorageClient.js
+++ static/remoteStorageClient.js
@@ -20,7 +20,7 @@ var remoteStorageClient = (function() {
     selfAccess1: { page: '/loggedIn.html', display:'pending 12/15', action: doSelfAccess1, next:{success: 'selfAccess2'}},
     selfAccess2: { page: '/loggedIn.html', display:'pending 13/15', action: doSelfAccess2, next:{success: 'selfAccess3'}},
     selfAccess3: { page: '/loggedIn.html', display:'pending 14/15', action: doSelfAccess3, next:{success: 'storing'}},
-    storing: { page: '/loggedIn.html', display:'pending 15/15', action: noStore, next:{success: 'pulling'}},
+    storing: { page: '/loggedIn.html', display:'pending 15/15', action: doStore, next:{success: 'pulling'}},
     allowRemoteStorage: { page: '/loggedIn.html', buttons:['allow', 'cancel']},
     pulling: { page: '/loggedIn.html', display:'pulling', buttons:['logout'], action: pull},
     error: { page: '/loggedIn.html', display:'error', buttons:['logout']}
@@ -48,6 +48,7 @@ var remoteStorageClient = (function() {
         }
         if(fsmInfo.action) {
           fsmInfo.action(sessionObj, function(result) {
+            console.log('got result "'+result+'" in step "'+sessionObj.state+'".');
             if(fsmInfo.next && fsmInfo.next[result]) {
               sessionObj.state = fsmInfo.next[result];
               localStorage.setItem('sessionObj', JSON.stringify(sessionObj));
@@ -63,6 +64,110 @@ var remoteStorageClient = (function() {
     } else {
       window.location = '/';
     }
+  }
+  function doSignIn(incomingObj, cb) {
+    var xhr=new XMLHttpRequest();
+    xhr.open('POST', 'http://libredocs.org/users', true);
+    xhr.onreadystatechange = function() {
+      if(xhr.readyState == 4) {
+        var sessionObj = {};
+        try {
+          sessionObj = JSON.parse(xhr.responseText);
+        } catch(e) {
+        }
+        if(sessionObj.ok) {
+          //this happens if we have a UserAddress record stored (for webfingerless user addresses)
+          if(audience=='http://libredocs.org') {//then the bearerToken is also directly stored in there
+            cb('found');
+          } else {
+            cb('needsAllow');
+          }
+        } else if(sessionObj.userAddress) {
+          cb('needsWebfinger');
+        } else {
+          alert('something went wrong! "'+xhr.responseText+'"['+xhr.status+']');
+          localStorage.clear();
+          window.location='/';
+        }
+      }
+    };
+    xhr.send(JSON.stringify({
+      audience: incomingObj.audience,
+      assertion: incomingObj.assertion
+    }));
+  }
+  function checkWebfinger(sessionObj, cb) {
+    if(sessionObj.state == 'wf1') {
+      require(['webfinger'], function(webfinger) {
+        webfinger.getAttributes(sessionObj.userAddress, {
+          onError: function(code, msg) {
+            if(code == 5) {
+              cb('fail');
+            }
+          }
+        }, function() {}, function(attr) {
+          sessionObj.attr = attr;
+          sessionObj.storageAddress = webfinger.resolveTemplate(attr.template, 'documents');
+          sessionObj.storageApi = attr.api;
+          localStorage.setItem('sessionObj', JSON.stringify(sessionObj));
+          cb('ok');
+        });
+      });
+    }
+  }
+  function enroll(sessionObj, cb) {
+    var userName = sessionObj.userAddress.replace(/-/g, '--').replace(/@/g, '-at-').replace(/\./g, '-dot-')+(sessionObj.userNameTry?'-'+sessionObj.userNameTry:'');
+    pimper.provision(userName, sessionObj.firstName, sessionObj.lastName, sessionObj.userAddress, sessionObj.adminPwd, function(result) {
+      if(result=='taken') {
+        console.log('Username '+userName+' is taken! :( trying a different one');
+        //sessionObj.userNameTry = (sessionObj.userNameTry?sessionObj.userNameTry:0)+1;
+        sessionObj.userNameTry = (sessionObj.userNameTry?sessionObj.userNameTry:14)+1;//skip straight to higher numbers, so debugging doesn't take that long
+        localStorage.setItem('sessionObj', JSON.stringify(sessionObj));
+        cb('taken');
+      } else if(result=='success') {
+        sessionObj.subdomain = userName;
+        sessionObj.proxy = 'yourremotestorage.net/CouchDB/proxy/';
+        localStorage.setItem('sessionObj', JSON.stringify(sessionObj));
+        cb('success');
+      } else {
+        alert('error '+result+' in provision step');
+        cb('error');
+      }
+    });
+  }
+  function ping(userName, proxy, counter, cb) {
+    //for(var i=0; i<=counter; i++) {
+    //  document.getElementById('status').innerHTML += '.';
+    //}
+    if(counter > 10) {
+      alert('your remote storage was not deployed within 10 pings. please try again.');
+      cb('error');
+    } else {
+      pimper.ping(userName, proxy, function(result) {
+        if(result=='ok') {
+          cb('success');
+        } else if(result='no') {
+          console.log('ping '+counter+'...');
+          ping(userName, proxy, counter+1, onSuccess);
+        } else {
+          alert('Error code '+result);
+          cb('error');
+        }
+      });
+    }
+  }
+  function doPing(sessionObj, cb) {
+    ping(sessionObj.subdomain, sessionObj.proxy, 0, cb);
+  }
+  function doSquat1(sessionObj, cb) {
+    pimper.createAdminUser1(sessionObj.subdomain+'.iriscouch.com', sessionObj.userAddress, sessionObj.adminPwd, function() {
+      cb('success');
+    });
+  }
+  function doSquat2(sessionObj, cb) {
+    pimper.createAdminUser2(sessionObj.subdomain+'.iriscouch.com', sessionObj.userAddress, sessionObj.adminPwd, function() {
+      cb('success');
+    });
   }
   function createDb(sessionObj, cb) {
     pimper.createDb(sessionObj.subdomain+'.iriscouch.com', sessionObj.userAddress, sessionObj.adminPwd, 'cors', function() {
@@ -124,104 +229,6 @@ var remoteStorageClient = (function() {
       }
     };
     xhr.send(JSON.stringify(sessionObj));
-  }
-  function ping(userName, proxy, counter, cb) {
-    //for(var i=0; i<=counter; i++) {
-    //  document.getElementById('status').innerHTML += '.';
-    //}
-    pimper.ping(userName, proxy, function(result) {
-      if(result=='ok') {
-        cb('success');
-      } else if(result='no') {
-        console.log('ping '+counter+'...');
-        ping(userName, proxy, counter+1, onSuccess);
-      } else {
-        alert('Error code '+result);
-      }
-    });
-  }
-  function doPing(sessionObj, cb) {
-    ping(sessionObj.subdomain, sessionObj.proxy, 0, cb);
-  }
-  function doSquat1(sessionObj, cb) {
-    pimper.createAdminUser1(sessionObj.subdomain+'.iriscouch.com', sessionObj.userAddress, sessionObj.adminPwd, function() {
-      cb('success');
-    });
-  }
-  function doSquat2(sessionObj, cb) {
-    pimper.createAdminUser2(sessionObj.subdomain+'.iriscouch.com', sessionObj.userAddress, sessionObj.adminPwd, function() {
-      cb('success');
-    });
-  }
-  function getUserNameForIrisCouch(userAddress, userNameTry) {
-    return userAddress.replace(/-/g, '--').replace(/@/g, '-at-').replace(/\./g, '-dot-')+(userNameTry?'-'+userNameTry:'');
-  }
-  function enroll(sessionObj, cb) {
-    var userName = getUserNameForIrisCouch(sessionObj.userAddress, sessionObj.userNameTry);
-    pimper.provision(userName, sessionObj.firstName, sessionObj.lastName, sessionObj.userAddress, sessionObj.adminPwd, function(result) {
-      if(result=='taken') {
-        console.log('Username '+userName+' is taken! :( trying a different one');
-        //sessionObj.userNameTry = (sessionObj.userNameTry?sessionObj.userNameTry:0)+1;
-        sessionObj.userNameTry = (sessionObj.userNameTry?sessionObj.userNameTry:14)+1;//skip straight to higher numbers, so debugging doesn't take that long
-        localStorage.setItem('sessionObj', JSON.stringify(sessionObj));
-        cb('taken');
-      } else if(result=='ok') {
-        sessionObj.subdomain = userName;
-        sessionObj.proxy = 'yourremotestorage.net/CouchDB/proxy/';
-        localStorage.setItem('sessionObj', JSON.stringify(sessionObj));
-        cb('success');
-      }
-    });
-  }
-  function checkWebfinger(sessionObj, cb) {
-    if(sessionObj.state == 'wf1') {
-      require(['webfinger'], function(webfinger) {
-        webfinger.getAttributes(sessionObj.userAddress, {
-          onError: function(code, msg) {
-            if(code == 5) {
-              cb('fail');
-            }
-          }
-        }, function() {}, function(attr) {
-          sessionObj.attr = attr;
-          sessionObj.storageAddress = webfinger.resolveTemplate(attr.template, 'documents');
-          sessionObj.storageApi = attr.api;
-          localStorage.setItem('sessionObj', JSON.stringify(sessionObj));
-          cb('ok');
-        });
-      });
-    }
-  }
-  function doSignIn(incomingObj, cb) {
-    var xhr=new XMLHttpRequest();
-    xhr.open('POST', 'http://libredocs.org/users', true);
-    xhr.onreadystatechange = function() {
-      if(xhr.readyState == 4) {
-        var sessionObj = {};
-        try {
-          sessionObj = JSON.parse(xhr.responseText);
-        } catch(e) {
-        }
-        if(sessionObj.ok) {
-          //this happens if we have a UserAddress record stored (for webfingerless user addresses)
-          if(audience=='http://libredocs.org') {//then the bearerToken is also directly stored in there
-            cb('found');
-          } else {
-            cb('needsAllow');
-          }
-        } else if(sessionObj.userAddress) {
-          cb('needsWebfinger');
-        } else {
-          alert('something went wrong! "'+xhr.responseText+'"['+xhr.status+']');
-          localStorage.clear();
-          window.location='/';
-        }
-      }
-    };
-    xhr.send(JSON.stringify({
-      audience: incomingObj.audience,
-      assertion: incomingObj.assertion
-    }));
   }
   function pull() {
   }
