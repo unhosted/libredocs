fb72d7ec685d251b86e16858314fe509c050d086
Jan-Christoph Borchardt <jan@unhosted.org>
2012-02-08 11:34:33 +0200

    displaying stuff inline

--- static/index.js
+++ static/index.js
@@ -1,13 +1,13 @@
 function go() {
   document.getElementById("signin-button").style.display='none';
-  document.getElementById("signin-loading").style.display='block';
+  document.getElementById("signin-loading").style.display='inline';
   navigator.id.get(function(assertion) {
     if(assertion) {
       remoteStorageClient.signIn('http://libredocs.org', assertion);
       window.location='loggedIn.html';
     }
     else {
-      document.getElementById("signin-button").style.display='block';
+      document.getElementById("signin-button").style.display='inline';
       document.getElementById("signin-loading").style.display='none';
     }
   });
@@ -18,6 +18,7 @@ if(localStorage.getItem('sessionObj')) {
 }
 document.getElementById('signin-button').onclick = go;
 document.getElementById('signin-loading').style.display='none';
+document.getElementById('signin-button').style.display='inline';
 
 (function($) {
   $(document).ready(function() {
