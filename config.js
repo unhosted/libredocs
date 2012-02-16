c554d2f03ba78852aa2cf99d388ee4d4b79dd3a0
Michiel de Jong <michiel@unhosted.org>
2012-02-16 19:31:18 +0100

    adding libredocs.org/proxy

--- config.js
+++ config.js
@@ -11,6 +11,7 @@ exports.config = {
   },
   pathHandler: {
     'libredocs.org/users': './users',
+    'libredocs.org/proxy': './proxy',
     'libredocs.org/useraddress': './useraddress',
     'libredocs.org/browserid-verifier': './browserid',
     'libredocs.org/provision': './provision',
---
+++ proxy/config.js
@@ -0,0 +1,8 @@
+exports.config = {
+  port: 80,
+  back: {
+    CouchDB: {
+      port: 5984
+    }
+  }
+};
