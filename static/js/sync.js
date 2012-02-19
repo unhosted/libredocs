902bb95c129c31d152dc6436c5299f5cc7481f04
Azul <azul@riseup.net>
2012-02-19 20:34:15 +0100

    moved document model into separate file

--- static/js/sync.js
+++ static/js/sync.js
@@ -1,72 +1,3 @@
-
-function fetchDocuments(cb){
-  cb(localGet('documents'), isRecent('documents'));
-  if(!isRecent('documents')) pullRemote('documents', function(err){
-    if(!err) fetchDocuments(cb);
-  });
-}
-
-// gets the etherpad document - contains text and all.
-function fetchDocument(id, cb){
-  if(id.split('$')[0] != currentUser()) return;
-  var key = 'pad:'+id;
-  local = localGet(key);
-  if(local){local.id = id; cb(local, isRecent(key));}
-  if(!isRecent(key)) pullRemote(key, function(err){
-    if(!err) fetchDocument(id, cb);
-  });
-}
-
-function saveDocument(doc, cb){
-  var documents = localGet('documents');
-  var index = localGet('index');
-  var key = doc.owner+'$'+doc.link;
-  documents[doc.id] = doc;
-  index[key] = doc.id;
-  localSet('documents', documents);
-  localSet('index', index);
-  async.parallel([
-    function(callback) {pushRemote('documents', callback)},
-    function(callback) {pushRemote('index', callback)}
-  ], cb);
-}
-
-function fetchDocumentId(owner, link, cb){
-  var key = owner + '$' + link;
-  var done = false;
-  var index = {};
-  var index = localGet('index');
-  if(index[key]) { cb(index[key]); return }
-  getRemoteClient(owner, function(client) {
-    client.get(encodeURIComponent('padInfo:'+link), function(err, data) {
-      console.log('fetched public '+link+' from '+owner+':"'+err+'"');
-      // if(data.movedTo)
-      if(err) {//the callback should deal with a null
-        cb(null);
-      } else {
-        saveDocument(data);
-        cb(data.id);
-      }
-    });
-  });
-}
-
-function publishDocument(doc, cb) {
-  var info = {
-    id: doc.id,
-    title: doc.title,
-    owner: doc.owner,
-    link: doc.link
-  }
-  var sessionObj = JSON.parse(localStorage.getItem('sessionObj'));
-  getMyRemoteClient('public', function(client) {
-    client.put('padInfo:'+doc.link, info, function(err, data) {
-      console.log('pushed info '+info+' for docLink "'+doc.link+'" - '+err+':"'+data+'"');
-      cb();
-    });
-  });
-}
-
 function localGet(key){
   var local = localStorage.getItem(key);
   if(local) {
@@ -142,7 +73,12 @@ function getRemoteClient(owner, cb){
 }
 
 function getOrFetchStorageInfo(user, cb) {
-  var storageOwners = localGet('storageOwners') || '{}';
+  var storageOwners = localGet('storageOwners') || {};
+  var sessionObj = JSON.parse(localStorage.sessionObj);
+  var currentUser = sessionObj.userAddress
+  if(!storageOwners[currentUser]) {
+    storageOwners[currentUser] = sessionObj.storageInfo
+  }
   if(storageOwners[user]) {
     cb(null, storageOwners[user]);
     return;
