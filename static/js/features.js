301555fa269da43e61c5919b197ea0a569c51853
Azul <azul@riseup.net>
2012-02-21 09:43:48 +0100

    I can't beliebe i do this to please ie...

--- static/js/features.js
+++ static/js/features.js
@@ -2,9 +2,9 @@ function alertFeature(feature) {
   var heading = feature + " support missing. "
   var message = "<p>Your browser does not support " + feature +". "
    + "Please use <a href='http://getfirefox.com'>one that does</a>!</p>";
-  document.getElementById('error-heading').innerHTML += heading;
-  document.getElementById('error-message').innerHTML += message;
-  document.getElementById('error').style.display = 'block';
+  $('#error-heading').append(heading);
+  $('#error-message').append(message);
+  $('#error').show();
 }
 (function($) {
   $(document).ready(function() {
