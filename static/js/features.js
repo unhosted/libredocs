e1db1a03b8565852bae21dd9404d050190f93309
Azul <azul@riseup.net>
2012-02-21 09:21:13 +0100

    allow for multiple feature missing messages

--- static/js/features.js
+++ static/js/features.js
@@ -1,9 +1,9 @@
 function alertFeature(feature) {
-  var heading = feature + " support missing."
-  var message = "Your browser does not support " + feature +". "
-   + "Please use <a href='http://getfirefox.com'>one that does</a>!";
-  document.getElementById('error-heading').innerHTML = heading;
-  document.getElementById('error-message').innerHTML = message;
+  var heading = feature + " support missing. "
+  var message = "<p>Your browser does not support " + feature +". "
+   + "Please use <a href='http://getfirefox.com'>one that does</a>!</p>";
+  document.getElementById('error-heading').innerHTML += heading;
+  document.getElementById('error-message').innerHTML += message;
   document.getElementById('error').style.display = 'block';
 }
 (function() {
