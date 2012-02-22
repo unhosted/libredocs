f26e834d6811fd34609d31fb088719bc8ce1f0f7
Michiel de Jong <michiel@unhosted.org>
2012-02-22 18:56:26 +0000

    refactor for making pre-signin webfinger possible

--- static/js/welcome2.js
+++ static/js/welcome2.js
@@ -1,68 +1,47 @@
-function getClientSideFakeFinger(email) {
-  var emailParts = email.split('@');
-  if(emailParts.length == 2) {
-    if(['uva.nl', 'unhosted.org', 'surf.unhosted.org'].indexOf(emailParts[1]) != -1) {
-      return {
-        userAddress: email,
-        storageInfo: {
-          type: 'simple',
-          auth: 'http://surf.unhosted.org:81/saml.php?user_id='+email,
-          template: 'http://surf.unhosted.org/'+email+'/{category}/'
-        },
-        state: 'allowRemoteStorage'
-      };
-    }
-  }
-  return false;
-}
-function go() {
+function signin() {
   document.getElementById("signin-button").style.display='none';
-  document.getElementById("signin-loading").style.display='inline';
+  document.getElementById("signin-checking").style.display='inline';
   var email = document.getElementById('email').value;
-  //cases:
-  //have token -> BrowserID
-  //have webfinger -> OAuth as advertised
-  //is a Dutch uni -> OAuth to surfnet
-  //nothing -> offer sign-up (with BrowserID)
-  var clientSideFakeFinger = getClientSideFakeFinger(email);
-  if(clientSideFakeFinger) {
-    localStorage.sessionObj = JSON.stringify(clientSideFakeFinger);
-    window.location.href = 'signin.html';
-  } else {
-    require(['http://unhosted.org/remoteStorage-0.4.3.js'], function(remoteStorage) {
-      remoteStorage.getStorageInfo(email, function(err, storageInfo) {
-        if(err) {
-          navigator.id.get(function(assertion) {
-            if(assertion) {
-              remoteStorageClient.signIn('http://libredocs.org', assertion);
-              window.location.href = 'signin.html';
-            } else {
-              document.getElementById("signin-button").style.display='inline';
-              document.getElementById("signin-loading").style.display='none';
-            }
-          }, {
-            requiredEmail: email
-          });
-        } else {
-          var sessionObj = { 
-            userAddress: email,
-            storageInfo: storageInfo,
-            state: 'allowRemoteStorage'
-          };
-          localStorage.sessionObj = JSON.stringify(sessionObj);
-          window.location.href = 'signin.html';
-        }
-      });
+  navigator.id.get(function(assertion) {
+    if(assertion) {
+      remoteStorageClient.signIn('http://libredocs.org', assertion);
+      window.location.href = 'signin.html';
+    }
+  }, {
+    requiredEmail: email
+  });
+}
+function allow() {
+  window.location.href = 'signin.html';
+}
+function checkEmail() {
+  var email = document.getElementById('email').value;
+  require(['http://unhosted.org/remoteStorage-0.4.3.js'], function(remoteStorage) {
+    remoteStorage.getStorageInfo(email, function(err, storageInfo) {
+      if(err) {
+        document.getElementById('signin-button').style.display='inline';
+      } else {
+        var email = document.getElementById('email').value;
+        var sessionObj = { 
+          userAddress: email,
+          storageInfo: storageInfo,
+          state: 'allowRemoteStorage'
+        };
+        localStorage.sessionObj = JSON.stringify(sessionObj);
+        document.getElementById('allow-button').style.display='inline';
+      }
+      document.getElementById('check-button').style.display='none';
     });
-  }
+  });
 }
 
 if(localStorage.getItem('sessionObj')) {
   window.location.href = 'signin.html';
 }
-document.getElementById('signin-button').onclick = go;
-document.getElementById('signin-loading').style.display='none';
-document.getElementById('signin-button').style.display='inline';
+document.getElementById('signin-button').onclick = signin;
+document.getElementById('allow-button').onclick = allow;
+document.getElementById('check-button').onclick = checkEmail;
+document.getElementById('check-button').style.display = 'block';
 
 (function($) {
   $(document).ready(function() {
