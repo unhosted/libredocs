48e9e6499e4e80bc8edb8def335af5a2f0f10a8c
Azul <azul@riseup.net>
2012-02-22 10:21:08 +0100

    Revert "remove fakefinger step"
    This reverts commit cdea43e5089bcaec72f2d48b630dd94c0bc26a2e.

--- static/js/remoteStorage-0.4.3.js
+++ static/js/remoteStorage-0.4.3.js
@@ -12,7 +12,7 @@ define([
     webfinger.getAttributes(userAddress, {
       allowHttpWebfinger: true,
       allowSingleOriginWebfinger: false,
-      allowFakefinger: false
+      allowFakefinger: true
     }, function(err, data) {
       cb(err, null);
     }, function(attributes) {
