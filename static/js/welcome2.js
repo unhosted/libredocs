5e2871c7663799168380832ae1694dcabf3b9330
Michiel de Jong <michiel@unhosted.org>
2012-02-22 13:02:04 +0000

    test welcome2 with test@surf.unhosted.org

--- static/js/welcome2.js
+++ static/js/welcome2.js
@@ -1,16 +1,8 @@
-function go() {
-  document.getElementById("signin-button").style.display='none';
-  document.getElementById("signin-loading").style.display='inline';
-  var email = document.getElementById('email').value;
+function getClientSideFakeFinger(email) {
   var emailParts = email.split('@');
   if(emailParts.length == 2) {
-    //cases:
-    //have token -> BrowserID
-    //have webfinger -> OAuth as advertised
-    //is a Dutch uni -> OAuth to surfnet
-    //nothing -> offer sign-up (with BrowserID)
-    if(emailParts[1] == 'uva.nl') {
-      localStorage.sessionObj = JSON.stringify({
+    if(['uva.nl', 'surf.unhosted.org'].indexOf(emailParts[1])) {
+      return {
         userAddress: email,
         storageInfo: {
           type: 'simple',
@@ -18,22 +10,36 @@ function go() {
           template: 'http://surf.unhosted.org/'+email+'/{category}/'
         },
         state: 'allowRemoteStorage'
-      });
-      window.location.href = 'signin.html';
-    } else {
-      navigator.id.get(function(assertion) {
-        if(assertion) {
-          remoteStorageClient.signIn('http://libredocs.org', assertion);
-          window.location.href = 'signin.html';
-        }
-        else {
-          document.getElementById("signin-button").style.display='inline';
-          document.getElementById("signin-loading").style.display='none';
-        }
-      }, {
-        requiredEmail: email
-      });
+      };
     }
+  }
+  return false;
+}
+function go() {
+  document.getElementById("signin-button").style.display='none';
+  document.getElementById("signin-loading").style.display='inline';
+  var email = document.getElementById('email').value;
+  //cases:
+  //have token -> BrowserID
+  //have webfinger -> OAuth as advertised
+  //is a Dutch uni -> OAuth to surfnet
+  //nothing -> offer sign-up (with BrowserID)
+  var clientSideFakeFinger = getClientSideFakeFinger(email);
+  if(clientSideFakeFinger) {
+    localStorage.sessionObj = JSON.stringify(clientSideFakeFinger);
+    window.location.href = 'signin.html';
+  } else {
+    navigator.id.get(function(assertion) {
+      if(assertion) {
+        remoteStorageClient.signIn('http://libredocs.org', assertion);
+        window.location.href = 'signin.html';
+      } else {
+        document.getElementById("signin-button").style.display='inline';
+        document.getElementById("signin-loading").style.display='none';
+      }
+    }, {
+      requiredEmail: email
+    });
   }
 }
 
