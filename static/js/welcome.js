d69c096655789d38a06927e25165d43b481b9a3c
Michiel de Jong <michiel@unhosted.org>
2012-02-23 20:03:10 +0000

    move welcome2.html into place as welcome.html and merging in changes on welcome.html since welcome2.html was created

--- static/js/welcome.js
+++ static/js/welcome.js
@@ -1,24 +1,47 @@
-function go() {
+function signin() {
   document.getElementById("signin-button").style.display='none';
-  document.getElementById("signin-loading").style.display='inline';
+  document.getElementById("signin-checking").style.display='inline';
+  var email = document.getElementById('email').value;
   navigator.id.get(function(assertion) {
     if(assertion) {
       remoteStorageClient.signIn('http://libredocs.org', assertion);
       window.location.href = 'signin.html';
     }
-    else {
-      document.getElementById("signin-button").style.display='inline';
-      document.getElementById("signin-loading").style.display='none';
-    }
+  }, {
+    requiredEmail: email
+  });
+}
+function allow() {
+  window.location.href = 'signin.html';
+}
+function checkEmail() {
+  var email = document.getElementById('email').value;
+  require(['http://unhosted.org/remoteStorage-0.4.3.js'], function(remoteStorage) {
+    remoteStorage.getStorageInfo(email, function(err, storageInfo) {
+      if(err) {
+        document.getElementById('signin-button').style.display='inline';
+      } else {
+        var email = document.getElementById('email').value;
+        var sessionObj = { 
+          userAddress: email,
+          storageInfo: storageInfo,
+          state: 'allowRemoteStorage'
+        };
+        localStorage.sessionObj = JSON.stringify(sessionObj);
+        document.getElementById('allow-button').style.display='inline';
+      }
+      document.getElementById('check-button').style.display='none';
+    });
   });
 }
 
 if(localStorage.getItem('sessionObj')) {
   window.location.href = 'signin.html';
 }
-document.getElementById('signin-button').onclick = go;
-document.getElementById('signin-loading').style.display='none';
-document.getElementById('signin-button').style.display='inline';
+document.getElementById('signin-button').onclick = signin;
+document.getElementById('allow-button').onclick = allow;
+document.getElementById('check-button').onclick = checkEmail;
+document.getElementById('check-button').style.display = 'block';
 
 (function($) {
   $(document).ready(function() {
