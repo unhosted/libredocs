a58517fbfdddafbce24959b36d75868ff1ddefd5
Michiel de Jong <michiel@unhosted.org>
2012-02-23 21:13:22 +0000

    more email checking

--- static/js/welcome.js
+++ static/js/welcome.js
@@ -14,12 +14,23 @@ function signin() {
 function allow() {
   window.location.href = 'signin.html';
 }
+function couldBeEmail(str) {
+  return /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(str);
+}
 function checkEmail() {
+  document.getElementById('allow-button').style.display='none';
+  document.getElementById('signin-button').style.display='none';
   var email = document.getElementById('email').value;
+  if(!couldBeEmail(email)) {
+   return;
+  }
+  document.getElementById('signin-checking').style.display='inline';
   require(['http://unhosted.org/remoteStorage-0.4.3.js'], function(remoteStorage) {
     remoteStorage.getStorageInfo(email, function(err, storageInfo) {
       if(err) {
+        document.getElementById('signin-checking').style.display='none';
         document.getElementById('signin-button').style.display='inline';
+        document.getElementById('allow-button').style.display='none';
       } else {
         var email = document.getElementById('email').value;
         var sessionObj = { 
@@ -28,9 +39,10 @@ function checkEmail() {
           state: 'allowRemoteStorage'
         };
         localStorage.sessionObj = JSON.stringify(sessionObj);
+        document.getElementById('signin-checking').style.display='none';
         document.getElementById('allow-button').style.display='inline';
+        document.getElementById('signin-button').style.display='none';
       }
-      document.getElementById('check-button').style.display='none';
     });
   });
 }
