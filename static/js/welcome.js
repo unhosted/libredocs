3b14081a356bc2e32d448bd1b25f15330c635909
Azul <azul@riseup.net>
2012-03-22 19:44:42 +0100

    seperating different functions in signup
    * we add the eventListener right away now - this way we do not get duplicates.
    * connected is a function in it's own right now - not a callback dependent on the email param
    
    I think we should copy the way the 5apps tutorial does this... with one function setting the email in localStorage and the other one setting the credentials. Getting the email from the field later on might yield wrong results if the user keeps changing it.

--- static/js/welcome.js
+++ static/js/welcome.js
@@ -27,64 +27,73 @@ define(function() {
       email = userAddressParts[0].substring(0,userAddressParts[0].indexOf('+'));
       alert('Using \''+email+'\' as your user address, the part behind the \'+\' is irrelevant here.');
     }
-    connect(email, ['documents', 'contacts', 'public'], function(err, storageInfo, token) {
-      var emailParts
-      if(err) {
-        alert('Sorry, that didn\'t work. Try signing up at 5apps.com or owncube.com, and then come back here and connect with [user]@[provider]');
-      } else {
-        document.getElementById('connect-address').disabled=true;
-        document.getElementById('connect-address').style.background='url("../images/loading.gif") no-repeat 1em center';
-        document.getElementById('connect-button').style.display='none';
-
-        var sessionObj;
-        try {
-          sessionObj = JSON.parse(localStorage.sessionObj);
-        } catch(e) {
-        }
-        if(!sessionObj) {
-          sessionObj={};
-        }
-        sessionObj.userAddress=email;
-        sessionObj.storageInfo=storageInfo;
-        sessionObj.bearerToken = token;
-        sessionObj.state = 'ready';
-        sessionObj.proxy = '';
-        sessionObj.clientSide = true;//prevents storing with migration fields in account.js
-        storeBearerToken(sessionObj.userAddress, sessionObj.bearerToken, function(result) {
-          if(result) {
-            localStorage.sessionObj = JSON.stringify(sessionObj);
-            init();
-            load();
-          } else {
-            document.getElementById('connect-address').disabled=false;
-            document.getElementById('connect-address').style.background='none';
-            document.getElementById('connect-button').style.display='inline';
-            alert('Sorry, that looks like an issue. Please come to http://webchat.freenode.net/?channels=unhosted and tell us about it');
-          }
-        });
-      }
-    }, false);
+    connect(email, ['documents', 'contacts', 'public']);
   }
-  function connect(userAddress, categories, cb) {
+
+  function connected(err, storageInfo, token) {
+    email=document.getElementById('connect-address').value;
+    var emailParts
+    if(err) {
+      alert('Sorry, that didn\'t work. Try signing up at 5apps.com or owncube.com, and then come back here and connect with [user]@[provider]');
+      return;
+    }
+    document.getElementById('connect-address').disabled=true;
+    document.getElementById('connect-address').style.background='url("../images/loading.gif") no-repeat 1em center';
+    document.getElementById('connect-button').style.display='none';
+    var sessionObj;
+    try {
+      sessionObj = JSON.parse(localStorage.sessionObj);
+    } catch(e) {
+    }
+    if(!sessionObj) {
+      sessionObj={};
+    }
+    sessionObj.userAddress=email;
+    sessionObj.storageInfo=storageInfo;
+    sessionObj.bearerToken = token;
+    sessionObj.state = 'ready';
+    sessionObj.proxy = '';
+    sessionObj.clientSide = true;//prevents storing with migration fields in account.js
+    storeBearerToken(sessionObj.userAddress, sessionObj.bearerToken, function(result) {
+      if(result) {
+        localStorage.sessionObj = JSON.stringify(sessionObj);
+        init();
+        load();
+      } else {
+        document.getElementById('connect-address').disabled=false;
+        document.getElementById('connect-address').style.background='none';
+        document.getElementById('connect-button').style.display='inline';
+        alert('Sorry, that looks like an issue. Please come to http://webchat.freenode.net/?channels=unhosted and tell us about it');
+      }
+    });
+  }
+
+  function connect(userAddress, categories) {
     var libPath = '';
     window.open(libPath+'/openDialog.html'
-      +'?userAddress='+encodeURIComponent(userAddress)
-      +'&categories='+encodeURIComponent(JSON.stringify(categories))
-      +'&libPath='+encodeURIComponent(libPath));
-    window.addEventListener('message', function(event) {
-      if(event.origin == location.protocol +'//'+ location.host) {
-        if(event.data.substring(0, 5) == 'conn:') {
-          var data = JSON.parse(event.data.substring(5));
-          cb(data.err, data.storageInfo, data.bearerToken);
-        }
-      }
-    }, false);
+        +'?userAddress='+encodeURIComponent(userAddress)
+        +'&categories='+encodeURIComponent(JSON.stringify(categories))
+        +'&libPath='+encodeURIComponent(libPath));
   }
+
   function loaded() {
     setTimeout(function() {
-      document.getElementById('connect-button').onclick = allow;
-    }, 100);//TODO: Gotta find a way, a better way, I'd better wait(x2)
+      var connectButton = $('#connect-button');
+      if(connectButton.attr('data-state') != 'active') {
+        connectButton.on('click', allow);
+        connectButton.attr('data-state', 'active');
+      }
+    }, 100);
   }
+
+  window.addEventListener('message', function(event) {
+    if(event.origin == location.protocol +'//'+ location.host) {
+    if(event.data.substring(0, 5) == 'conn:') {
+      var data = JSON.parse(event.data.substring(5));
+      connected(data.err, data.storageInfo, data.bearerToken);
+    }
+  }
+  }, false);
 
   return {
     loaded: loaded
