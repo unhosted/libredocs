43cd3b3711fa69692359e1866cb70043a6232caa
Azul <azul@riseup.net>
2012-03-27 10:50:43 +0200

    handing over the bearerToken directly first steps
    Also included jasmine and sinon and did this test driven.

---
+++ static/js/ConnectPad.js
@@ -0,0 +1,41 @@
+connectPad = (function() {
+  var host = "http://surf.unhosted.org:82";
+  var api = "/ownapi/1/";
+
+  function urlFor(functionName, params){
+    var query = [];
+    for (var p in params) {
+      query.push(encodeURIComponent(p) + "=" + encodeURIComponent(params[p]));
+    }
+    return host+api+functionName+"?"+query.join("&");
+  }
+
+  function paramsFor(sessionObj){
+    return {
+      userAddress: sessionObj.userAddress,
+      storageInfo: JSON.stringify(sessionObj.storageInfo),
+      bearerToken: sessionObj.bearerToken
+    }
+  }
+
+  function connect(sessionObj, callback){
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', urlFor("connect", paramsFor(sessionObj)));
+    xhr.onreadystatechange = function() {
+      if(xhr.readyState==4) {
+        if(xhr.status==201 || xhr.status==200) {
+          callback(true);
+        } else {
+          callback(false);
+        }
+      }
+    };
+    xhr.send();
+  }
+
+  return {
+    urlFor: urlFor,
+    paramsFor: paramsFor,
+    connect: connect
+  };
+})();
---
+++ static/spec/ConnectPadSpec.js
@@ -0,0 +1,55 @@
+describe("ConnectPad", function() {
+
+  var xhr;
+  var requests;
+
+  beforeEach(function() {
+    xhr = sinon.useFakeXMLHttpRequest();
+    requests = [];
+
+    xhr.onCreate = function (request) {
+      requests.push(request);
+    };
+  });
+
+  afterEach(function() {
+    xhr.restore();
+  });
+
+
+  it("builds the right api urls", function() {
+    var funct = "myTestFunction";
+    var params = {a: "value", another: "one"};
+    expect(connectPad.urlFor(funct, params)).toEqual("http://surf.unhosted.org:82/ownapi/1/myTestFunction?a=value&another=one");
+  });
+
+  it("builds params from session Obj", function () {
+    var sessionObj = {
+      userAddress: "my@email.net",
+      storageInfo: {template: "template in here", oauth: "oauth string in here"},
+      bearerToken: "SECRETBEARERTOKENHERE"
+    };
+    var params = connectPad.paramsFor(sessionObj);
+    expect(params.bearerToken).toEqual("SECRETBEARERTOKENHERE");
+    expect(JSON.parse(params.storageInfo).template).toEqual("template in here");
+    expect(params.userAddress).toEqual("my@email.net");
+  });
+
+  // integration test
+  it("sends a request to the url created", function () {
+    var sessionObj = {
+      userAddress: "my@email.net",
+      storageInfo: {template: "template in here", oauth: "oauth string in here"},
+      bearerToken: "SECRETBEARERTOKENHERE"
+    };
+    var callback = sinon.spy();
+    connectPad.connect(sessionObj, callback); // handing empty session obj - we stub it anyway
+    expect(requests.length).toEqual(1);
+    expect(requests[0].url).toMatch(/\/connect\?/);
+    expect(requests[0].method).toEqual("GET");
+    requests[0].respond(200, { "Content-Type": "application/json" },
+      '[{ "id": 12, "comment": "Hey there" }]');
+    //TODO: make this use a proper err, status return code
+    expect(callback).toHaveBeenCalledWith(true);
+  });
+});
